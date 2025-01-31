e8748362c2d613fb533062857fa6f3ce5a162ac2
Qadir Babayev <qadir.babayev@setclapp.com>
2025-01-31 14:38:41 +0400

    first commit

---
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -0,0 +1,28 @@
+import logo from "../imgs/logo.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+
+            <Link to="/" className="flex-none w-10">
+                <img src={logo} className="w-full"/>
+            </Link>
+
+            <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw]">
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey"
+                />
+
+                <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+            </div>
+
+            
+            
+        </nav>
+    )
+}
+
+export default Navbar;
