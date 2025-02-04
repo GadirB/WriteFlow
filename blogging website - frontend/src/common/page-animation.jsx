f9c31a065290acaeae5d00ea35fe7a7fd25a24dd
Qadir Babayev <babayev00qadir@gmail.com>
2025-02-04 11:51:28 +0400

    SignIn and SignUp Animations

--- blogging website - frontend/src/common/page-animation.jsx
+++ blogging website - frontend/src/common/page-animation.jsx
@@ -1,8 +1,16 @@
-const AnimationWrapper = ({ children }) => {
+import { AnimatePresence, motion } from "framer-motion";
+
+const AnimationWrapper = ({ children, keyValue, initial = { opacity: 0 }, animate = { opacity: 1 }, transition = {duration: 1 }, className }) => {
     return (
-        <div>
+        <motion.div
+            key={keyValue}
+            initial={initial}
+            animate={animate}
+            transition={transition}
+            className={className}
+        >
             { children }
-        </div>
+        </motion.div>
     )
 }
 
