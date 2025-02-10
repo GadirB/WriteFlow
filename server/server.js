d9bf7bae20bd4c2264bdb2d5aea897a0f7c03918
Qadir Babayev <babayev00qadir@gmail.com>
2025-02-10 17:39:46 +0400

    Authorization

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
-import User from './Schema/User.js'
+import User from './Schema/User.js';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
 
 const server = express();
 let PORT = 3000;
@@ -14,47 +16,69 @@ server.use(express.json());
 
 mongoose.connect(process.env.DB_LOCATION, {
     autoIndex: true
-})
+});
 
-server.post("/signup", (req, res) => {
-
-    let { fullname, email, password } = req.body;
-
-    //validating the data from frontend
-    if(fullname.length < 3){
-        return res.status(403).json({ "error": "Fullname must be at least 3 letters long" })
-    } 
-    if(!email.length){
-        return res.status(403).json({ "error": "Enter Email" })
+const formatDatatoSend = (user) => {
+    
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY)
+    return{
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname
     }
-    if(!emailRegex.test(email)){
-        return res.status(403).json({ "error": "Email is invalid" })
-    }
-    if(!passwordRegex.test(password)){
-        return res.status(403).json({ "error": "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters" })
-    }
+}
 
-    bcrypt.hash(password, 10, (err, hashed_password) => {
+const generateUsername = async (email) => {
+    let username = email.split("@")[0];
 
-        let username = email.split("@")[0];
+    let usernameExists = await User.exists({ "personal_info.username": username }).then((result) => result)
+
+    usernameExists ? username += nanoid().substring(0, 5) : "";
+
+    return username
+}
+
+server.post("/signup", async (req, res) => {
+    try {
+        let { fullname, email, password } = req.body;
+
+        // Validating the data from frontend
+        if (fullname.length < 3) {
+            return res.status(403).json({ "error": "Fullname must be at least 3 letters long" });
+        }
+        if (!email.length) {
+            return res.status(403).json({ "error": "Enter Email" });
+        }
+        if (!emailRegex.test(email)) {
+            return res.status(403).json({ "error": "Email is invalid" });
+        }
+        if (!passwordRegex.test(password)) {
+            return res.status(403).json({ "error": "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letter" });
+        }
+
+        // Hashing password
+        const hashed_password = await bcrypt.hash(password, 10);
+        console.log(hashed_password);
+
+        let username = await generateUsername(email);
 
         let user = new User({
-            personal_info: { fullname, email, password: hashed_password, username}
-        })
+            personal_info: { fullname, email, password: hashed_password, username }
+        });
 
         user.save().then((u) => {
-            return res.status(200).json({ user: u })
-        })
-        .catch(err => {
-            return res.status(500).json({ "error": err.message })
+            return res.status(200).json(formatDatatoSend(u))
         })
 
-        console.log(hashed_password)
-    })
-
-    return res.status(200).json({ "status": "okay" })
-})
+    } catch (err) {
+        if (err.code == 11000) {
+            return res.status(500).json({ "error": "Email already exists" });
+        }
+        return res.status(500).json({ "error": err.message });
+    }
+});
 
 server.listen(PORT, () => {
-    console.log('listening on port -> ' + PORT);
-})
+    console.log('Listening on port -> ' + PORT);
+});
