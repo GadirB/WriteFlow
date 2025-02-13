bec7e30e397c277cfe390636101bfb854ae67514
Qadir Babayev <babayev00qadir@gmail.com>
2025-02-13 14:54:22 +0400

    SignIn

--- server/server.js
+++ server/server.js
@@ -79,6 +79,33 @@ server.post("/signup", async (req, res) => {
     }
 });
 
+server.post("/signin", (req, res) => {
+    let { email, password } = req.body;
+
+    User.findOne({ "personal_info.email": email })
+    .then((user) => {
+        if(!user){
+            return res.status(403).json({ "error": "Email not found" })
+        }
+
+        bcrypt.compare(password, user.personal_info.password, (err, result) =>{
+            if(err) {
+                return res.status(403).json({ "error": "Error occurred while login please try again" });
+            }
+
+            if(!result){
+                return res.status(403).json({ "error": "Incorrect Password" });
+            } else{
+                return res.status(200).json(formatDatatoSend(user));
+            }
+        })
+    })
+    .catch(err => {
+        console.log(err.message);
+        return res.status(500).json({ "error": err.message })
+    })
+})
+
 server.listen(PORT, () => {
     console.log('Listening on port -> ' + PORT);
 });
